fc567d14f6137ee2ad43c3eda1a5d3dd88bb6f89
Alexandra Konstantinovskaia <sk-armyansk@mail.ru>
2016-09-09 23:14:09 +0300

    changes js

--- js5-6/script.js
+++ js5-6/script.js
@@ -15,70 +15,48 @@ start.addEventListener("click" , function() {
 	
 	if (run === false) {
 		begin();
+		run = true;
 		start.textContent = 'Pause';
-		run = true;	
 	} else {
 		clearInterval(timer);
-		start.textContent = 'Start';
 		run = false;
+		start.textContent = 'Start';
 	}
 });
 
-
+reset.addEventListener("click" , function() {
+	end();
+});
 
 function begin() {
-	timer = setInterval( render , 100);
+	timer = setInterval( render , 10);
 }
+
 function render() {
-	  	time += 10;
+  	time += 10;
+	
+	var ms = time % 1000;
+	var s = (time - ms) / 1000 % 60;
+	var m = ((time - ms) / 1000 - s) / 60 % 60;
+	var h = (((time - ms) / 1000 - s) / 60 - m) / 60;
 
-	var ms = Math.floor(time % 1000) / 10; 
- 	if(ms<10) {
-		milisecond.textContent = "0" + ms;
- 	}
- 	else {
- 		milisecond.textContent = ms;
- 	}
-
- 	var s = Math.floor(time / 100 % 60);
- 	// var s = (time % 6000) / 1000;
- 	if(s<10) {
- 		seconds.textContent = "0" + s;
- 	}
- 	else {
- 		seconds.textContent = s;
- 	}
-
- 	var m = Math.floor(time / 100 / 60);
- 	
- 	if(m<10) {
- 		minutes.textContent = "0" + m;
- 	}
- 	else {
- 		minutes.textContent = m;
- 	}
-
- 	var h = Math.floor (time / 100 / 60 / 60);
- 	if(h<10) {
- 		hours.textContent = "0" + h;
- 	}
- 	else {
- 		hours.textContent = h;
- 	}
-console.log(time);
+	milisecond.textContent = format(ms/10, 2);
+	seconds.textContent = format(s, 2);
+	minutes.textContent = format(m, 2);
+	hours.textContent = format(h, 2);
 }
 
+function format(num, len) {
+	return ('0000' + num).substr(-2, len);
+}
 
-reset.addEventListener("click" , end);
 function end() {
 	clearInterval(timer);
-	start.textContent = 'Start';
 	run = false;
 	time = 0;
+	start.textContent = 'Start';
 	milisecond.textContent = "00";
 	seconds.textContent = "00";
 	minutes.textContent = "00";
 	hours.textContent = "00";
-
 }
-
